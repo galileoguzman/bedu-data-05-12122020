d2e2ac90fd0ed4c284e9137fcccc3230ee8afc09
galileoguzman <galileoguzman@gmail.com>
2020-12-12 14:06:11 -0600

    Added new lambda

---
+++ 03_lambda_functions.py
@@ -0,0 +1,17 @@
+def add_uno(numero):
+    return numero + 1
+
+suma_uno = lambda x : x + 1
+
+print(type(add_uno))
+print(type(suma_uno))
+
+# print(add_uno(5))
+# print(add_uno(7))
+# print(add_uno(2))
+# print(add_uno(6))
+
+# print(suma_uno(5))
+# print(suma_uno(7))
+# print(suma_uno(2))
+# print(suma_uno(6))
